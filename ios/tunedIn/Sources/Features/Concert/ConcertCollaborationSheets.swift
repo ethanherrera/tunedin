62caf0954860227eb767d688f868cfdbbfbdf7eb
Ethan <ethanherrera@berkeley.edu>
2026-07-12 18:05:52 -0700

    Fix iOS lint file limit

--- ios/tunedIn/Sources/Features/Concert/ConcertCollaborationSheets.swift
+++ ios/tunedIn/Sources/Features/Concert/ConcertCollaborationSheets.swift
@@ -992,28 +992,3 @@ struct ConcertCommentsView: View {
     }
   }
 }
-
-private struct CollaboratorMonogram: View {
-  let member: ConcertCollaborator
-  let size: CGFloat
-
-  var body: some View {
-    Text(String(member.displayName.prefix(1)).uppercased())
-      .font(.system(size: size * 0.38, weight: .black, design: .rounded))
-      .foregroundStyle(TunedInDesign.actionForeground)
-      .frame(width: size, height: size)
-      .background(TunedInDesign.accentTint, in: Circle())
-  }
-}
-
-private struct CommentMonogram: View {
-  let comment: ConcertComment
-
-  var body: some View {
-    Text(String(comment.displayName.prefix(1)).uppercased())
-      .font(.caption.weight(.black))
-      .foregroundStyle(TunedInDesign.actionForeground)
-      .frame(width: 32, height: 32)
-      .background(TunedInDesign.accentTint, in: Circle())
-  }
-}
